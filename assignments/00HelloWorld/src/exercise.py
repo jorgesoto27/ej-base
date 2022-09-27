eca8a14b60be7af72d9e5ba1e18f10c0c28b33f5
jorgesoto27 <111130816+jorgesoto27@users.noreply.github.com>
2022-09-27 13:54:44 +0000

    Listo :)

--- assignments/00HelloWorld/src/exercise.py
+++ assignments/00HelloWorld/src/exercise.py
@@ -1,6 +1,17 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    n = int(input())
+    lista1 = []
+    for i in range (n):
+        x = int(input())
+        lista1.append(x)
+    lista2 = []
+    for i in range (0, len(lista1)):
+        y = lista1 [i]**2
+        lista2.append(y)
+
+    print(f"{lista1}")
+    print(f"{lista2}")
 
 if __name__=='__main__':
     main()
